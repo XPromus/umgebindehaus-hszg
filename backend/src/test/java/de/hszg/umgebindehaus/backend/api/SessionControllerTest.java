7b38e1fc98717474db866f9d5ca618c6515335b5
Christopher <hilgi45@gmail.com>
2021-12-14 11:05:15 +0100

    Vorbereitung der Testklasse.

--- backend/src/test/java/de/hszg/umgebindehaus/backend/api/SessionControllerTest.java
+++ backend/src/test/java/de/hszg/umgebindehaus/backend/api/SessionControllerTest.java
@@ -1,5 +1,7 @@
 package de.hszg.umgebindehaus.backend.api;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
@@ -9,6 +11,32 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 public class SessionControllerTest extends AbstractTestNGSpringContextTests {
 
+    @Test
+    public void createSession() {
 
+        RestAssured.port = 8080;
+
+    }
+
+    @Test
+    public void editSession() {
+
+        RestAssured.port = 8080;
+
+    }
+
+    @Test
+    public void getSessionData() {
+
+        RestAssured.port = 8080;
+
+    }
+
+    @Test
+    public void closeSession() {
+
+        RestAssured.port = 8080;
+
+    }
 
 }
