376f7550308396667cc623d0dfb609affa5a4885
Christopher <hilgi45@gmail.com>
2021-12-14 10:39:15 +0100

    Neue Test Klasse

---
+++ backend/src/test/java/de/hszg/umgebindehaus/backend/api/SessionControllerTest.java
@@ -0,0 +1,14 @@
+package de.hszg.umgebindehaus.backend.api;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@SpringBootTest (
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+)
+
+public class SessionControllerTest extends AbstractTestNGSpringContextTests {
+
+
+
+}
