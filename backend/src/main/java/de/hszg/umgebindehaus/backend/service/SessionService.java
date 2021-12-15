f1c6b9732c5638a47b91b9c525a9d3b7a110372c
Max.-F. Helm <maximilian-felix.helm@stud.hszg.de>
2021-12-15 10:10:53 +0100

    SessionService now uses ConcurrentHashMap instead of explicit synchronisation

--- backend/src/main/java/de/hszg/umgebindehaus/backend/service/SessionService.java
+++ backend/src/main/java/de/hszg/umgebindehaus/backend/service/SessionService.java
@@ -7,23 +7,25 @@ import de.hszg.umgebindehaus.backend.data.model.Session;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class SessionService{
 
     private final UniqueWordGenerator idGenerator;
 
-    private final Map<String, Session> sessions = new HashMap<>();
+    private final Map<String, Session> sessions;
 
     public SessionService(UniqueWordGenerator idGenerator){
         this.idGenerator = idGenerator;
+
+        sessions = new ConcurrentHashMap<>();
     }
 
     @NotNull
     public Session createSession(){
-        synchronized (sessions){
+        synchronized (sessions){// this should still be full synchronized to ensure unique keys
             String id = idGenerator.nextWord();
             while(sessions.containsKey(id))// should not happen but just to make sure
                 id = idGenerator.nextWord();
@@ -38,20 +40,16 @@ public class SessionService{
 
     @NotNull
     public Session getSessionById(@NotNull String id){
-        synchronized (sessions){
-            var session = sessions.get(id);
-            if(session == null)
-                throw new ResourceNotFoundException(String.format("no session with id %s registered", id));
-            return session;
-        }
+        var session = sessions.get(id);
+        if(session == null)
+            throw new ResourceNotFoundException(String.format("no session with id %s registered", id));
+        return session;
     }
 
     public void removeSession(@NotNull String id){
-        synchronized (sessions){
-            if(!sessions.containsKey(id))
-                throw new ResourceNotFoundException(String.format("no session with id %s registered", id));
-            sessions.remove(id);
-        }
+        if(!sessions.containsKey(id))
+            throw new ResourceNotFoundException(String.format("no session with id %s registered", id));
+        sessions.remove(id);
     }
 
     public void editSession(@NotNull String sessionId, @NotNull ScenePropertiesEdit changes){
