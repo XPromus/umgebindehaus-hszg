2b6f0c5df9250534bda6b70f1d562f76e3a540a4
Zafkiel1312 <tim.doering1@gmx.de>
2022-02-08 14:58:46 +0100

    If-Cascade has been deleted, frontend will now get the full message from the Exception

--- backend/src/main/java/de/hszg/umgebindehaus/backend/api/error/CustomExceptionHandler.java
+++ backend/src/main/java/de/hszg/umgebindehaus/backend/api/error/CustomExceptionHandler.java
@@ -1,12 +1,12 @@
 package de.hszg.umgebindehaus.backend.api.error;
 
-import de.hszg.umgebindehaus.backend.api.error.JSONValueExceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
@@ -18,27 +18,17 @@ public class CustomExceptionHandler {
     public void handleEntityNotFound() {}
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public void handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        String exceptionMessage = e.getMessage();
+    public ResponseEntity<ErrorMessage> handleHttpMessageNotReadableException(HttpMessageNotReadableException e, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                e.getMessage(),
+                request.getDescription(false).replace("uri=", "")
+        );
 
-        if (exceptionMessage.contains("newName")) {
-            throw new NewNameValueException("There is an error with the value of 'newName'");
-        } else if (exceptionMessage.contains("newTime")) {
-            throw new NewTimeValueException("There is an error with the value of 'newTime'");
-        } else if (exceptionMessage.contains("newTimeScale")) {
-            throw new NewTimeScaleValueException("There is an error with the value of 'newTimeScale'");
-        } else if (exceptionMessage.contains("newAutomaticWeather")) {
-            throw new NewAutomaticWeatherValueException("There is an error with the value of 'newAutomaticWeather'");
-        } else if (exceptionMessage.contains("newAutomaticTime")) {
-            throw new NewAutomaticTimeValueException("There is an error with the value of 'newAutomaticTime'");
-        } else if (exceptionMessage.contains("newWeatherWindDirection")) {
-            throw new NewWeatherWindDirectionValueException("There is an error with the value of 'newWeatherWindDirection'");
-        } else if (exceptionMessage.contains("newWeatherWindSpeed")) {
-            throw new NewWeatherWindSpeedValueException("There is an error with the value of 'newWeatherWindSpeed'");
-        } else if (exceptionMessage.contains("newWeatherCloudiness")) {
-            throw new NewWeatherCloudinessValueException("There is an error with the value of 'newWeatherCloudiness'");
-        } else {
-            throw new FaultyJSONException("The JSON-String could not be converted to an Object");
-        }
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(message);
     }
 }
